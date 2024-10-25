4b6af090c0f17b123f75baf8d885d65ed76dde2f
Pritesh Chaturvedi <priteshchoubey567@gmail.com>
2024-10-25 19:26:31 +0530

    Create popMethod.js

---
+++ popMethod.js
@@ -0,0 +1,11 @@
+// Remove Elements from Array using 
+// JavaScript Array pop() Method
+
+let arr = ["Apple", "Banana", "Orange", "Mango"];
+
+// Popping the last element from the array 
+let popped = arr.pop();
+
+console.log("Removed Element:", popped);
+console.log("Remaining Array Elements:", arr);
+console.log("Array Length:", arr.length);
